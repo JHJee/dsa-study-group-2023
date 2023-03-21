59962d5b25c871fdb14bb2eb6931abbb36ac5fe3
Evelyn Jihye Choe <47475637+evelynjhc@users.noreply.github.com>
2023-03-21 16:32:30 +0900

    Create LC_102_binary-tree-level-order-traversal.py

---
+++ EV/LeetCode/LC_102_binary-tree-level-order-traversal.py
@@ -0,0 +1,25 @@
+'''
+    runtime: 35 ms Beats 73.36%
+    memory: 14.2 MB Beats 75.72%
+'''
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level_size = len(queue)
+            level_vals = []
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_vals.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level_vals)
+    
+        return result
