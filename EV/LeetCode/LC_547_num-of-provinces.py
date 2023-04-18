d987a92e8cab712d34560e95374be71dcb533fda
evelynjhc <chlwlgp132@gmail.com>
2023-04-18 20:46:35 +0900

    week 09

---
+++ EV/LeetCode/LC_547_num-of-provinces.py
@@ -0,0 +1,26 @@
+'''
+    runtime: 254 ms Beats 11.5%
+    memory: 14.2 MB Beats 94.25%
+'''
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        provinces = 0
+
+        n = len(isConnected)
+        def bfs(node):
+            queue = deque([node])
+
+            while queue:
+                curr = queue.popleft()
+                for neighbor in range(n):
+                    if isConnected[curr][neighbor] == 1:
+                        isConnected[curr][neighbor] = isConnected[neighbor][curr] = 0
+                        queue.append(neighbor)
+
+        for i in range(n):
+            if isConnected[i][i] == 1:
+                provinces += 1
+                bfs(i)
+
+        return provinces
---
+++ EV/LeetCode/LC_797_all-paths-from-source-to-target.py
@@ -0,0 +1,22 @@
+'''
+    runtime: 100 ms Beats 72.61%
+    memory: 15.8 MB Beats 33.35%
+'''
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        paths = []
+        n = len(graph)
+        source, target = 0, n - 1
+        stack = [(source, [source])]
+
+        while stack:
+            node, path = stack.pop()
+            if node == target:
+                paths.append(path)
+                continue
+                
+            for neighbor in graph[node]:
+                stack.append((neighbor, path + [neighbor]))
+
+        return paths
